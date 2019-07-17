b90bea8a2a0f90dcc1fbb11b078a4284a90456c8
luoshuai <lsxyz7@gmail.com>
2019-07-17 11:32:27 +0800

    add git submodule

--- clients/web/src/main.js
+++ clients/web/src/main.js
@@ -1,7 +1,18 @@
 import account from '@packages/account';
+import HttpFetchClient from 'http-fetch-client';
+
+const request = new HttpFetchClient();
 
 document.getElementById('application').innerHTML = `
   web: ${account.getName()}, welcome.
-`
+`;
+
+
+request.get('/')
+  .use(() => {
+    document.getElementById('application').innerHTML = document.getElementById('application').innerHTML + `
+      get / completed;
+    `;
+  })
 
 // Array.from(['aaa']);
